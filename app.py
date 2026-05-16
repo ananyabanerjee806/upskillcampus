9d01d5ef09708e92c981cecc679af0d4463b6061
ananyabanerjee806 <ananyabanerjee806@gmail.com>
2026-05-16 13:49:52 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import google.generativeai as genai
+
+st.set_page_config(
+    page_title="Ananya Chatbot",
+    page_icon="🤖"
+)
+
+# Configure Gemini API
+genai.configure(api_key="AIzaSyBhoLIN-5GUedEPmUDqGFX45J8hSwhIJIk")
+
+# Load model
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+st.title("Ananya AI ChatBot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display previous messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# User input
+prompt = st.chat_input("Type your message")
+
+if prompt:
+
+    # Store user message
+    st.session_state.messages.append(
+        {
+            "role": "user",
+            "content": prompt
+        }
+    )
+
+    # Show user message
+    with st.chat_message("user"):
+        st.write(prompt)
+
+    # Generate AI response
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+
+            response = model.generate_content(prompt)
+
+            ai_response = response.text
+
+            st.write(ai_response)
+
+            # Store AI response
+            st.session_state.messages.append(
+                {
+                    "role": "assistant",
+                    "content": ai_response
+                }
+            )
